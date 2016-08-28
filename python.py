d30cedba9999b675f824a8c9c389bbb2cb1cf38f
Karl <Kouralr@gmail.com>
2016-08-27 18:55:51 -0700

    Initial Commit

---
+++ python.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask import request
+from rest_functions import *
+import tweepy
+import time
+import json
+#from keys import classes/functions and etc
+from keys import *
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_tempalte("index.html")
+
+#RESULTS PAGE
+@app.route('/results', methods=['POST'])
+def my_form_post():
+    tmp_data = []
+    text = request.form
+    keyword = request.form["text"]  #Grab input data from the form
+    for tweet in tweepy.Cursor(api.search, q=keyword).items(1):
+        tmp_data.append(tweet)
+    lastest_tweet = len(tmp_data) - 1
+    tweet_info = print_tweet(tmp_data[lastest_tweet])
+    return render_template("results.html", dog = tweet_info)
+
+if __name__ == "__main__":
+    app.run()
---
+++ twitter_pack/rest_twitter.py
@@ -0,0 +1,40 @@
+import tweepy
+import time
+import json
+#from keys import classes/functions and etc
+from keys import *
+
+
+#Sets up twitter authentication
+auth = tweepy.OAuthHandler(consumer_key(), consumer_secret())
+auth.set_access_token(access_token(), access_token_secret())
+api = tweepy.API(auth)
+
+#Returns list of tweet_objects of your own timeline
+#public_tweets = api.home_timeline()
+
+keyword = "#"
+keyword += raw_input('Enter your hashtag: #')
+#data = api.search(q=keyword)    #returns an array of tweet objects
+num_of_results = input('How many results do you want? ')    #Number of tweets we want
+numOfAttributes = 4     #Number of attributes we are tracking
+
+data = []
+processed_data = [[0 for i in xrange(numOfAttributes)] for i in xrange(num_of_results)]
+
+#Adds all the tweets onto data *Each tweet is the json obj tweet
+#FOR WHAT TO DOT OPERATE. LOOK AT TWITTER DOCUMENTATION FOR TWEETS.
+for tweet in tweepy.Cursor(api.search, q=keyword).items(num_of_results):
+        data.append(tweet)
+
+def print_tweet(tweet):
+    print "@%s - %s (%s)" % (tweet.user.screen_name, tweet.user.name, tweet.created_at)
+    print tweet.text
+    print "Tweet coordinates: " + str(tweet.coordinates) 
+    print "Author Location: " + tweet.user.location
+    print "Retweet Count: " + str(tweet.retweet_count) 
+    #The dot operator allows for EASY dict-value getting
+
+#process_data(data)
+for tweet in data:
+    print_tweet(tweet)
