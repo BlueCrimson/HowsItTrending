66acd209b1dcd7d0ee0bdbbe28a6f41a7e008cb1
Karl <Kouralr@gmail.com>
2016-09-02 01:07:57 -0700

    Fixed listOfTweets dictionary/list problem

--- rest_functions.py
+++ rest_functions.py
@@ -11,18 +11,19 @@ auth.set_access_token(access_token(), access_token_secret())
 api = tweepy.API(auth)
 
 def get_tweets(listOfTweets, keyword, numOfTweets):
+ 
     for tweet in tweepy.Cursor(api.search, q=keyword).items(numOfTweets):
-        listOfTweets.append({'Screen Name': tweet.user.screen_name})
-        listOfTweets.append({'User Name': tweet.user.name})
-        listOfTweets.append({'Tweet Created At': unicode(tweet.created_at)})
-        listOfTweets.append({'Tweet Text': tweet.text})
-        listOfTweets.append({'User Location': unicode(tweet.user.location)})
-        listOfTweets.append({'Tweet Coordinates': unicode(tweet.coordinates)})
-        listOfTweets.append({'Retweet Count': unicode(tweet.retweet_count)})
-        listOfTweets.append({'Retweed': unicode(tweet.retweeted)})
-        listOfTweets.append({'Phone Type': unicode(tweet.source)})
-        #listOfTweets.append({'Sensitive': })
-        listOfTweets.append({'Language': unicode(tweet.lang)})
+        dict_ = {'Screen Name': tweet.user.screen_name,
+            'User Name': tweet.user.name,
+            'Tweet Created At': unicode(tweet.created_at),
+            'Tweet Text': tweet.text,
+            'User Location': unicode(tweet.user.location),
+            'Tweet Coordinates': unicode(tweet.coordinates),
+            'Retweet Count': unicode(tweet.retweet_count),
+            'Retweeted': unicode(tweet.retweeted),
+            'Phone Type': unicode(tweet.source)
+                }
+        listOfTweets.append(dict_)   
     return listOfTweets
 
 def getRetweetAvg(listOfTweets):
@@ -32,31 +33,42 @@ def getRetweetAvg(listOfTweets):
         if tweet.get('Retweet Count'):
             numOfRetweets += int(tweet.get('Retweet Count'))
         count += 1
-    return float(numOfRetweets/count)
+    if count == 0:
+        return 0
+    else:
+        return float(numOfRetweets/count)
 
-def getRetweetTotal(listOfTweets):
+def getRetweetCount(listOfTweets):
     numOfRetweets = 0
     for tweet in listOfTweets:
         if tweet.get('Retweet Count'):
             numOfRetweets += int(tweet.get('Retweet Count'))
     return numOfRetweets
-"""
-def getRetweetPercent(listOfTweets):
-    peopleRetweet = 0
-    peopleTotal = 0
-    for tweet in listOfTweets:
-        if tweet.get('Retweet Count'):
-            if int(tweet.get('Retweet Count')) > 0:
-                peopleRetweet += int(tweet.get('Retweet Count'))
-                peopleTotal += int(tweet.get('Retweet Count'))
-            else:
-    return numOfRetweets
-"""
 
-#listOfTweets = [{}]
+def getRetweetPercent(listOfTweets):
+    peopleTotal = len(listOfTweets)
+    peopleRetweet = 0
+    for tweet in listOfTweets:
+        if tweet.get('Retweeted'):
+            if (tweet.get('Retweeted') == "True"):
+                peopleRetweet += 1
+    return float(peopleRetweet/peopleTotal)
+
+def phoneTypePercent(listOfTweets):
+    peopleTotal = len(listOfTweets)
+    phoneTypes = {}
+    #print listOfTweets[2]
+    for tweet in listOfTweets:
+        print tweet['Phone Type']
+
+    #return #phoneTypes
+
+
+
+listOfTweets = []
 #get_tweets(listOfTweets, "ok", 10)
-#listOfTweets = get_tweets(listOfTweets, '#PokemonGo', 200)
-#print process_tweets(listOfTweets)
+listOfTweets = get_tweets(listOfTweets, '#PokemonGo', 2)
+phoneTypePercent(listOfTweets)
 
 #for i in listOfTweets:
 #    if i.get('Retweet Count'):
@@ -66,4 +78,3 @@ def getRetweetPercent(listOfTweets):
 #process_data(data)
 #for tweet in data:
 #    print_tweet(tweet)
-
