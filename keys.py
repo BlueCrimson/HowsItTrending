f8c4ecd1296b4964f944e0a213188e435478f449
Karl <krummler96@gmail.com>
2016-09-24 12:40:58 -0700

    Delete keys.py

--- keys.py
+++
@@ -1,9 +0,0 @@
-#KEYS
-def consumer_key():
-    return 'qnR6rNmtAtqFOWt7sBbAq2MAY'
-def consumer_secret():
-    return 'EtPJTIRhLDCbN5u1oUumzJJq1SrY5bXgxX7q28xeUzCx6QtA9A'
-def access_token():
-    return '752673922399801344-fMQUutKVKNLGsw6NgyQwBOhyc7nwQme'
-def access_token_secret():
-    return 'AxPTgSbh3Pw35pWUULCrK0q0DmDEnTx8Z8dSx5kOMVRCA'
