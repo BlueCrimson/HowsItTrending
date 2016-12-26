a2829d4a8ba5c275c2fedad3d3d3d02697971ca1
Karl <Kouralr@gmail.com>
2016-12-26 00:05:22 -0800

    Fixed callback problem

--- static/device_used.js
+++ static/device_used.js
@@ -1,11 +1,12 @@
 function getDeviceUsed(devicePercent) {
     console.log("Get Device Used -- LOADED")
       // Load the Visualization API and the piechart package.
-      google.charts.load('current', {'packages':['corechart']});
+      google.charts.load('current', {'packages':['corechart','bar']});
         
       // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
+      google.charts.setOnLoadCallback(drawPieChart);
+
+      function drawPieChart() {
         var jsonData = $.ajax({
             url: "getData.php",
             dataType: "json",
@@ -21,18 +22,54 @@ function getDeviceUsed(devicePercent) {
             result.push([i, json_data [i]]);
 
         //Creating our data obj that will be presented as a pie chart
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Device Type');
-        data.addColumn('number', 'Number of people ');
-        data.addRows(result);
+        var deviceData = new google.visualization.DataTable();
+        deviceData.addColumn('string', 'Device Type');
+        deviceData.addColumn('number', 'Number of people ');
+        deviceData.addRows(result);
+
         //Options of our data
-        var options = {
+        var deviceOptions = {
         title: 'Device Used',
+        // Min % for slice (.05 = 5%), otherwise its casted to other
         sliceVisibilityThreshold: .05
       };
         // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
+        var deviceChart = new google.visualization.PieChart(document.getElementById('devicePieChart'));
+        deviceChart.draw(deviceData, deviceOptions);
+        //TEST TEST TEST
+        
+      var tweetData = new google.visualization.DataTable();
+      // Set up the X axis
+      tweetData.addColumn('timeofday', 'Time');
 
+      // Set up the Y axis
+      tweetData.addColumn('number', 'Number of Tweets');
+
+      // Insert data entries
+      tweetData.addRows([
+        [[8, 30, 45], 5],
+        [[9, 0, 0], 10],
+        [[10, 0, 0, 0], 12],
+        [[10, 45, 0, 0], 13],
+        [[11, 0, 0, 0], 15],
+        [[12, 15, 45, 0], 20],
+        [[13, 0, 0, 0], 22],
+        [[14, 30, 0, 0], 25],
+        [[15, 12, 0, 0], 30],
+        [[16, 45, 0], 32],
+        [[16, 59, 0], 42]
+      ]);
+
+      // Options for the bar chart
+      var tweetOptions = google.charts.Bar.convertOptions({
+        title: 'Tweets Through the day',
+        height: 300
+      });
+
+      // Initialize the chart
+      var tweetChart = new google.charts.Bar(document.getElementById('tweetsOverTime'));
+
+      // Draw with our options
+      tweetChart.draw(tweetData, tweetOptions);
       }
 }
--- static/tweetsOverTime.js
+++ static/tweetsOverTime.js
@@ -1,28 +1,41 @@
-function works() {
-	console.log("tweetsOverTime -- LOADED")	
-}
-function getTweetsOverTime(tweetsOverTime){
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-            data.addColumn('number', 'Day');
+    // Load bar chart
+    google.charts.load('current', {'packages':['bar']});
 
-      function drawChart() {
-      	 // Create our data table out of JSON data loaded from server.
-        var json_data = devicePercent;
-        
-        //Converts json data into an array
-        var result = [];
-        for(var i in json_data)
-        result.push([i, json_data [i]]);
+    // Set callback so it runs after Google Charts loads
+    google.charts.setOnLoadCallback(drawChart);
 
-        var options = {
-          title: 'Company Performance',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
+    function drawChart() {
+      var tweetData = new google.visualization.DataTable();
+      // Set up the X axis
+      tweetData.addColumn('timeofday', 'Time');
 
-        var chart = new google.visualization.LineChart(document.getElementById('chart_dive'));
+      // Set up the Y axis
+      tweetData.addColumn('number', 'Number of Tweets');
 
-        chart.draw(data, options);
-      }
-}
+      // Insert data entries
+      tweetData.addRows([
+        [[8, 30, 45], 5],
+        [[9, 0, 0], 10],
+        [[10, 0, 0, 0], 12],
+        [[10, 45, 0, 0], 13],
+        [[11, 0, 0, 0], 15],
+        [[12, 15, 45, 0], 20],
+        [[13, 0, 0, 0], 22],
+        [[14, 30, 0, 0], 25],
+        [[15, 12, 0, 0], 30],
+        [[16, 45, 0], 32],
+        [[16, 59, 0], 42]
+      ]);
+
+      // Options for the bar chart
+      var tweetOptions = google.charts.Bar.convertOptions({
+        title: 'Tweets Through the day',
+        height: 300
+      });
+
+      // Initialize the chart
+      var tweetChart = new google.charts.Bar(document.getElementById('tweetsOverTime'));
+
+      // Draw with our options
+      tweetChart.draw(tweetData, tweetOptions);
+    }
